bb5603de8c2ecff666f1b4bc4ec421f22a6031e0
siahray <josiahraydevera0@gmail.com>
2025-08-08 00:34:49 +0800

    images for album

--- rolbux ft. among use/general.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.drop');
-    
-    hamburger.addEventListener('click', function() {
-        nav.classList.toggle('active');
-    });
-
-});
